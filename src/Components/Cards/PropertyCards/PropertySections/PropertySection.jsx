e87ad9135303e9a630233e79b687c937c6d886a1
70ni <4thedevelopments@gmail.com>
2024-05-21 22:36:04 +0530

    Property changes

--- src/Components/Cards/PropertyCards/PropertySections/PropertySection.jsx
+++ src/Components/Cards/PropertyCards/PropertySections/PropertySection.jsx
@@ -5,6 +5,10 @@ import "./propertysec.css";
 function PropertySection() {
   return (
     <div className="property-section-wraper">
+      <div className="property-Header-container">
+        <div className="subHeader">Recently Added df</div>
+        <div className="cardtext">Recently Added</div>
+      </div>
       <div className="property-sec-container">
         <Property />
         <Property />
