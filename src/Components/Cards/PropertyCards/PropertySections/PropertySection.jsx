2cc6da25ac4f6821c8354fc4e9fa0d29a5788fc1
70ni <4thedevelopments@gmail.com>
2024-05-22 15:46:26 +0530

    Properties Added

--- src/Components/Cards/PropertyCards/PropertySections/PropertySection.jsx
+++ src/Components/Cards/PropertyCards/PropertySections/PropertySection.jsx
@@ -6,14 +6,16 @@ function PropertySection() {
   return (
     <div className="property-section-wraper">
       <div className="property-Header-container">
-        <div className="subHeader">Recently Added df</div>
-        <div className="cardtext">Recently Added</div>
+        <div className="Header">Recently Added</div>
+        <div className="cardtext">See more</div>
       </div>
-      <div className="property-sec-container">
-        <Property />
-        <Property />
-        <Property />
-        <Property />
+      <div className="prop-content-flex">
+        <div className="property-sec-container">
+          <Property />
+          <Property />
+          <Property />
+          <Property />
+        </div>
       </div>
     </div>
   );
