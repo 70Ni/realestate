017e21590ca2162941ec7c21caa122c2f2896362
70ni <4thedevelopments@gmail.com>
2024-05-22 15:47:14 +0530

    Changes on country  card

--- src/Components/Cards/CountryCards/CountryCard/Counties.jsx
+++ src/Components/Cards/CountryCards/CountryCard/Counties.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../CountryCard/Countries.css'
 import America from "../../../Images/BannerImage.png";
+import France from "../../../Images/France.png";
+import spain from "../../../Images/Spain.png";
+import london from "../../../Images/London.png";
 
-function Counties({text}) {
+function Counties({text,img}) {
   return (  
     <div className="country-card-wrapper"
-    style={{ backgroundImage: `url(${America})` }}>
+    style={{ backgroundImage: `url(${img})` }}>
         <div className="country-name subHeader">{text}</div>
     </div>
   )
