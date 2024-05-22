0726032ae198068434f2cc1243686b7d3fd3c5a8
70ni <4thedevelopments@gmail.com>
2024-05-22 15:49:40 +0530

    Banner based on condition check

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import Banner from './Components/Banners/Banner';
-import Counties from './Components/Cards/CountryCards/CountryCard/Counties';
-import CountrySection from './Components/Cards/CountryCards/CountrySection';
-import Property from './Components/Cards/PropertyCards/PropertyCards/Property';
-import PropertySection from './Components/Cards/PropertyCards/PropertySections/PropertySection';
-import Footer from './Components/Footer/Footer';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import Banner from "./Components/Banners/Banner";
+import Counties from "./Components/Cards/CountryCards/CountryCard/Counties";
+import CountrySection from "./Components/Cards/CountryCards/CountrySection";
+import Property from "./Components/Cards/PropertyCards/PropertyCards/Property";
+import PropertySection from "./Components/Cards/PropertyCards/PropertySections/PropertySection";
+import Footer from "./Components/Footer/Footer";
+import BackImg02 from "./Components/Images/Banner02.png";
+import BannerImg from "./Components/Images/BannerImage.png";
+import Search from "./Components/Search/Search";
+import Accordion from "./Components/Accordion/Accordion";
+const search = "search";
 
 function App() {
   return (
     <div className="App">
-      {/* <Banner /> */}
-      {/* <CountrySection /> */}
-      {/* <PropertySection /> */}
-      <Footer />
+      <div className="Hero-wrapper">
+        {/* <Search /> */}
+        <Banner backImg={BannerImg} action={search}  />
+        <CountrySection />
+        <PropertySection />
+        <Banner backImg={ BackImg02 } />
+        <Footer />
+      </div>
     </div>
   );
 }
