af1505c0ca6ee903f2754bd465ceb31c5f821e4e
70ni <4thedevelopments@gmail.com>
2024-05-22 15:47:38 +0530

    County images changed

--- src/Components/Cards/CountryCards/CountrySection.jsx
+++ src/Components/Cards/CountryCards/CountrySection.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Counties from "./CountryCard/Counties";
 import "./ConntrySec.css";
-
+import america from "../../Images/BannerImage.png";
+import france from "../../Images/France.png";
+import spain from "../../Images/Spain.png";
+import london from "../../Images/London.png";
 function CountrySection() {
   return (
     <div className="countries-wrapper">
@@ -9,10 +12,10 @@ function CountrySection() {
         We are available in many well-known countries
       </div>
       <div className="countries-section">
-        <Counties text="AMERICA" />
-        <Counties text="SPAIN" />
-        <Counties text="LONDON" />
-        <Counties text="FRANCE" />
+        <Counties text="AMERICA" img ={america} />
+        <Counties text="SPAIN"  img ={spain}/>
+        <Counties text="LONDON"  img ={london}/>
+        <Counties text="FRANCE" img ={france} />
       </div>
     </div>
   );
